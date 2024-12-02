697d08d392307485d4e5b3790d251d6aad63f6fb
Michael Crawford <mbcrawfo@gmail.com>
2024-12-02 03:20:01 -0500

    Project setup, because node is insane

--- src/day-01/index.ts
+++ src/day-01/index.ts
@@ -1,1 +1,6 @@
-console.log("Day 1");
+import path from "node:path";
+import { readLines } from "../utilities/readLines";
+
+const lines = await readLines(path.resolve(__dirname, "input.txt"));
+
+console.log(lines.slice(0, 5));
---
+++ src/utilities/readLines.ts
@@ -0,0 +1,18 @@
+import * as fs from "node:fs";
+import * as readline from "node:readline/promises";
+
+export const readLines = async (filePath: string): Promise<string[]> => {
+    const fileStream = fs.createReadStream(filePath);
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity,
+    });
+
+    const lines: string[] = [];
+    for await (const line of rl) {
+        lines.push(line);
+    }
+
+    return lines;
+};
